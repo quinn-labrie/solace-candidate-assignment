d0c84d8b064a3af7131cb721cac7e80243e38728
quinn <whyquinn@gmail.com>
2025-09-07 14:36:56 -0500

    fixed typing in db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,14 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema"; // Import your schema
 
-const setup = () => {
+const setup = (): PostgresJsDatabase<typeof schema> => {
   if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
+    throw new Error("DATABASE_URL is not set");
   }
 
-  // for query purposes
   const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
+  const db = drizzle(queryClient, { schema });
   return db;
 };
 
